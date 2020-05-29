0fae7de6832f523b06a8dc5f49f89530fe352183
Aric Huang <arichuang@gmail.com>
2020-05-29 15:50:06 -0700

    60s

--- spike/run.sh
+++ spike/run.sh
@@ -5,11 +5,12 @@ do
 	echo "Checking supervisor"
 	curl -X GET --header "Content-Type:application/json" \
 	    "$BALENA_SUPERVISOR_ADDRESS/ping?apikey=$BALENA_SUPERVISOR_API_KEY"
+	echo
 
 	echo "Triggering supervisor update"
-	curl -X POST --header "Content-Type:application/json" \
+	curl -iX POST --header "Content-Type:application/json" \
 	    --data '{"force": true}' \
 	    "$BALENA_SUPERVISOR_ADDRESS/v1/update?apikey=$BALENA_SUPERVISOR_API_KEY"
 
-	sleep 10
+	sleep 60
 done
